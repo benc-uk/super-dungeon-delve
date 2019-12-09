6972b0b9a6a589de2a46118c9c3b54b76136c015
Ben Coleman <becolem@microsoft.com>
2019-12-09 14:50:01 +0000

    Blah

--- actors/player.gd
+++ actors/player.gd
@@ -4,26 +4,49 @@ var time:= 0.0
 var rng: = RandomNumberGenerator.new()
 var light_old_pos = Vector2(8, 8)
 
+export var base_speed = Vector2(120, 120)
+var _velocity: = Vector2.ZERO
+var _speed = Vector2.ZERO
+var _last_dir: = Vector2.ZERO
+
 func _ready():
 	rng.randomize()
 	
 func _physics_process(delta: float):
 	time += delta
 	if time > 500000: time = 0
-	if Input.is_action_pressed("down"):
-		move_and_slide(Vector2(0, 100))
-	if Input.is_action_pressed("up"):
-		move_and_slide(Vector2(0, -100))
-	if Input.is_action_pressed("left"):
-		move_and_slide(Vector2(-100, 0))
-		$Sprite.flip_h = true
-	if Input.is_action_pressed("right"):
-		move_and_slide(Vector2(100, 0))		
-		$Sprite.flip_h = false
 
+	# Momentum code, not sure I like it
+	#if Input.is_action_pressed("move_left"): _speed = base_speed
+	#if Input.is_action_pressed("move_right"): _speed = base_speed
+	#if Input.is_action_pressed("move_up"): _speed = base_speed
+	#if Input.is_action_pressed("move_down"): _speed = base_speed
+	#var direction = get_direction()
+	#if direction.x == 0 and direction.y == 0: 
+	#	direction = _last_dir
+	#else:
+	#	_last_dir = direction
+	#_speed *= 0.8
+	#if _speed.x < 0.0001 and _speed.y < 0.0001: _speed = Vector2.ZERO
+	
+	# No momentum 		
+	var direction = get_direction()
+	_speed = base_speed
+	
+	if direction.x < 0: $Sprite.flip_h = true
+	if direction.x > 0: $Sprite.flip_h = false
+	
+	_velocity = move_and_slide(direction * _speed, Vector2.UP, false, 4, 0.78, false)
+	
 	# light flicker		
 	$Light2D.texture_scale = 0.19 + (cos(time * 9) * 0.005)
-	$Light2D.energy = 2.8 + (cos(time * 6) * 0.1)
+	$Light2D.energy = 2.8 + (cos(time * 2) * 0.1)
+	
+func get_direction() -> Vector2:
+	return Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	)
 	
 func _on_LightTimer_timeout():
 	var light_new_pos = Vector2(rand_range(4, 12), rand_range(4, 12))
